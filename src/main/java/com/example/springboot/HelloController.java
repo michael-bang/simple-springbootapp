48480a0b430662b0bdeae9a4810ba6b1064387f1
Michael Bang <mbang@redhat.com>
2021-09-02 23:20:09 +0200

    Updated HelloController to version 2

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 	@GetMapping("/")
 	public String index() {
         String st = "<html><head><title>Greetings from Spring Boot version 1</title></head>\n";
-        st = st + "<body><H1>Greetings from version 1</H1></body></html>";
+        st = st + "<body><H1>Greetings from version 2</H1></body></html>";
 
 		return st;
 	}
